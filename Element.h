eb7fd9863580164d81a1eb0f66859d4b64cd222e
elcerdo <georges.wbushjr@laposte.net>
2010-04-23 10:38:46 +0200

    abstarct class

--- Element.h
+++ Element.h
@@ -1,22 +1,21 @@
-#include"vecteur3.h"
-#include <vector>
-
 #ifndef __ELEMENT_0ff0
 #define __ELEMENT_0ff0
+
+#include"vecteur3.h"
+#include <vector>
 
 typedef Vecteur3<float> Vecteur3f;
 
 class Element {
-    public:
-        // Verifie si il y a une intersection avec l'element
-        virtual bool isIntersection(Vecteur3f origine, Vecteur3f direction) { return false; };
-        // Si une intersection a été détectée, il est possible d'en demander la distance. Sinon, exception
-        virtual float distanceIntersection()  { return 0; };
-        // Si l'élément émet de la lumière. 0 si pas de lumière
-        
-        virtual void deplacer(Vecteur3f d) { };
+public:
+    // Verifie si il y a une intersection avec l'element
+    virtual bool isIntersection(Vecteur3f origine, Vecteur3f direction) =0;
 
-        virtual float luminosite(std::vector<Element* >) { return 0;};
+    // Si une intersection a été détectée, il est possible d'en demander la distance. Sinon, exception
+    virtual float luminosite(std::vector<Element* >) =0;
+    virtual float distanceIntersection()  =0;
+    
+    virtual void deplacer(Vecteur3f d) =0;
 };
 
 #endif
