9ff775e626ff40ed6ff7415382a7d35a7c5b61f8
Simon M <github@marache.net>
2017-03-08 19:57:04 +0000

    translate to french

--- main.cpp
+++ main.cpp
@@ -4,18 +4,19 @@
 #include "plane.h"
 #include "material.h"
 #include <algorithm>
+
 static const int SCREEN_INIT_ERROR = 5;
 static const int RETURN_OK = 0;
 
-static const int LARGEUR = 512;
-static const int HAUTEUR = 384;
+static const int SCREEN_WIDTH = 512;
+static const int SCREEN_HEIGH = 384;
 
 static const float tanx = tanf(static_cast<float> (M_PI) / 4.0f);
-static const float tany = tanf(static_cast<float> (HAUTEUR) / static_cast<float> (LARGEUR) * static_cast<float> (M_PI) / 4.0f);
+static const float tany = tanf(static_cast<float> (SCREEN_HEIGH) / static_cast<float> (SCREEN_WIDTH) * static_cast<float> (M_PI) / 4.0f);
 
 Direction definirDirection(Direction &direction, float x, float y) {
-    direction[0] = tanx * (2.f * x - LARGEUR) / (LARGEUR);
-    direction[1] = tany * (2.f * y - HAUTEUR) / (HAUTEUR);
+    direction[0] = tanx * (2.f * x - SCREEN_WIDTH) / (SCREEN_WIDTH);
+    direction[1] = tany * (2.f * y - SCREEN_HEIGH) / (SCREEN_HEIGH);
     direction[2] = 1.f;
     direction.makeUnitVector();
     return direction;
@@ -31,25 +32,25 @@ Pixel lightToPixel(Light l) {
 
 int main(int argc, char *argv[]) {
     Uint32 *screen; //This pointer will reference the backbuffer 
-    screen = InitVideo(LARGEUR, HAUTEUR);
+    screen = InitVideo(SCREEN_WIDTH, SCREEN_HEIGH);
     if (screen == NULL) exit(SCREEN_INIT_ERROR);
 
     LightRay camera_ray(Position(0, 0, -3), Direction(0, 0, 1));
-    World monde;
+    World world;
 
     Sphere* mov = new Sphere(Position(3, 3, 10), 5, Material::glass());
-    monde.elements.push_back(mov);
-    monde.elements.push_back(new Sphere(Position(-3, 3, 10), 5, Material::RedPlastic()));
-    monde.elements.push_back(new Sphere(Position(-3, -3, 10), 4, Material::silver()));
-    monde.elements.push_back(new Plane());
+    world.elements.push_back(mov);
+    world.elements.push_back(new Sphere(Position(-3, 3, 10), 5, Material::RedPlastic()));
+    world.elements.push_back(new Sphere(Position(-3, -3, 10), 4, Material::silver()));
+    world.elements.push_back(new Plane());
 
     while (1) {
 
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_MOUSEMOTION) {
-                monde.globalLight.direction[0] += 0.01f * (event.motion.xrel);
-                monde.globalLight.direction[1] += 0.01f * (event.motion.yrel);
+                world.globalLight.direction[0] += 0.01f * (event.motion.xrel);
+                world.globalLight.direction[1] += 0.01f * (event.motion.yrel);
                 //camera_ray.origin[0] += 0.01f
                 //	* (event.motion.xrel);
                 //camera_ray.origin[1] += 0.01f * (event.motion.yrel);
@@ -66,14 +67,14 @@ int main(int argc, char *argv[]) {
         mov->translate(Direction(0.f, 0.f, 0.1f * (1 - 2 * (((int) (t1 / 2000.0f)) % 2))));
 
         //#pragma omp parallel
-        for (int x = 0; x < LARGEUR; x++) for (int y = 0; y < HAUTEUR; y++) {
+        for (int x = 0; x < SCREEN_WIDTH; x++) for (int y = 0; y < SCREEN_HEIGH; y++) {
                 definirDirection(camera_ray.direction, static_cast<float> (x), static_cast<float> (y));
-                Light value = monde.rayTracing(camera_ray, NULL);
+                Light value = world.rayTracing(camera_ray, NULL);
                 definirPixel(screen, x, y, lightToPixel(value));
             }
 
         showImage(screen);
-        printf("temps: %1ums\n", SDL_GetTicks() - t1);
+        printf("time: %1ums\n", SDL_GetTicks() - t1);
     }
 
     freeRenderer(screen);
